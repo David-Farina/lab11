e8be5823292cda475ce402cafb4b8fbf642994e8
rahulnimmagaddasai <rahul.nimmagadda@ufl.edu>
2026-04-08 18:18:44 -0400

    Changes to calc.py

--- test_calculator.py
+++ test_calculator.py
@@ -29,7 +29,7 @@ class TestCalculator(unittest.TestCase):
     def test_divide_by_zero(self): # 1 assertion
     #     # call division function inside, example:
         with self.assertRaises(ZeroDivisionError):
-            divide(0,5)
+            divide(5,0)
     #     fill in code
 
     def test_logarithm(self): # 3 assertions
