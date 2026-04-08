1e7d51a0ce43457018fb939c812bb01d7f6294c4
David Farina <dfarina999@hotmail.com>
2026-04-08 16:57:33 -0400

    made changes to calculator

--- calculator.py
+++ calculator.py
@@ -4,8 +4,30 @@ calculator.py
 
 One function per operation, in order.
 """
+import math
 # First example
 def add(a, b): 
-    pass
+    return a + b
 
+def subtract(a, b):
+    return a - b
 
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    try:
+        return a / b
+    except ZeroDivisionError:
+        print("Cannot divide by 0")
+        return None
+
+def logarithm(a, b):
+    try:
+        return math.log(a, b)
+    except Exception as e:
+        print(e)
+        return None
+
+def exponent(a, b):
+    return a ** b
