656dff41d65e1d1f5825ad6cb47555b377f710bb
rahulnimmagaddasai <rahul.nimmagadda@ufl.edu>
2026-04-08 17:30:17 -0400

    Changes to calc.py

--- calculator.py
+++ calculator.py
@@ -1,3 +1,4 @@
+
 """
 calculator.py
 - Defines functions used to create a simple calculator
@@ -5,7 +6,25 @@ calculator.py
 One function per operation, in order.
 """
 # First example
-def add(a, b): 
-    pass
+def add(a, b):
+    a + b
+def sub(a,b):
+    a - b
+def mul(a,b):
+    a*b
+def div(a,b):
+    try:
+        a/b
+    except ZeroDivisionError:
+        raise ZeroDivisionError
+def log(a,b):
+    try:
+        math.log(b,a)
+    except ValueError:
+        raise ValueError
+
+def exp(a,b):
+    a**b
+
 
 
