a773fcaa643c239995bdd16388a31a360f5105dc
David Farina <dfarina999@hotmail.com>
2026-04-08 17:33:20 -0400

    modified calculator

--- calculator.py
+++ calculator.py
@@ -6,6 +6,16 @@ One function per operation, in order.
 """
 import math
 # First example
+def square_root(a):
+    try:
+        return math.sqrt(a)
+    except ValueError:
+        print("Value cannot be < 0")
+        return None
+
+def hypotenuse(a, b):
+    return math.hypot(a, b)
+
 def add(a, b): 
     return a + b
 
