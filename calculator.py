95c3b1e8b10ba39f0b965ed37aeb22bdd81338a7
David Farina <dfarina999@hotmail.com>
2026-04-08 17:46:22 -0400

    calculator.py

--- calculator.py
+++ calculator.py
@@ -1,11 +1,22 @@
+import math
 """
 calculator.py
 - Defines functions used to create a simple calculator
 
 One function per operation, in order.
 """
-import math
 # First example
+<<<<<<< HEAD
+def square_root(a):
+    try:
+        return math.sqrt(a)
+    except ValueError:
+        print("Value cannot be < 0")
+        return None
+
+def hypotenuse(a, b):
+    return math.hypot(a, b)
+
 def add(a, b): 
     return a + b
 
@@ -16,6 +27,15 @@ def multiply(a, b):
     return a * b
 
 def divide(a, b):
+=======
+def add(a, b):
+    a + b
+def sub(a,b):
+    a - b
+def mul(a,b):
+    a*b
+def div(a,b):
+>>>>>>> e7c027bd18dc87710a7f03a9f2395c473b28af66
     try:
         a/b
     except ZeroDivisionError:
